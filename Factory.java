71c85e288ed6868eb41df3bec69a41e417d8f1ba
Asiri Senith <asirisenith@gmail.com>
2020-08-15 12:21:44 +0530

    Factory Design Pattern Created

---
+++ Factory.java
@@ -0,0 +1,37 @@
+
+interface Mamel{
+    void drinkMilk();
+}
+
+class Human implements Mamel{
+    public void drinkMilk() {
+        System.out.println("Human Drinking");
+    }
+}
+
+class Monkey implements Mamel{
+    public void drinkMilk() {
+        System.out.println("Monkey Drinking");        
+    }
+}
+
+class Factory{
+
+    public static void main(final String[] args) {
+        Mamel m = MamelFactory("Human");
+        m.drinkMilk();
+    }
+
+    public static Mamel MamelFactory(String m) {
+
+        if (m == "Human"){
+            return new Human();
+        }
+        else if (m=="Monkey") {
+            return new Monkey();
+        } else {
+            System.out.println("Nothing Match this String value");
+        }
+        return null;
+    }
+}
