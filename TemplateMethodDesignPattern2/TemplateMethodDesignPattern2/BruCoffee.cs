15496faf21f4bdc5fe219ff0b62d602c11b21d58
AsiriZenith <asirisenith@gmail.com>
2021-06-10 23:19:40 +0530

    Template Method Design Pattern Example 2

---
+++ TemplateMethodDesignPattern2/TemplateMethodDesignPattern2/BruCoffee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethodDesignPattern2
+{
+    class BruCoffee : CoffeeTemplate
+    {
+        protected override void AddCoffeePowder()
+        {
+            Console.WriteLine("Water Boild");
+        }
+
+        protected override void AddMilk()
+        {
+            Console.WriteLine("Milk Added");
+        }
+
+        protected override void AddSugar()
+        {
+            Console.WriteLine("Sugar Added");
+        }
+
+        protected override void BoilWater()
+        {
+            Console.WriteLine("Bru Coffee Powder Added");
+        }
+    }
+}
