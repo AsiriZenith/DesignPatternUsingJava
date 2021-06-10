020e0113b8613f2a9820543c0cb4d1d091e992ea
AsiriZenith <asirisenith@gmail.com>
2021-06-10 22:12:31 +0530

    Strategy Design Pattern second example

---
+++ StrategyDesignPattern2/StrategyDesignPattern2/ITravelStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyDesignPattern2
+{
+    interface ITravelStrategy
+    {
+        void GotoAirport();
+    }
+}
---
+++ StrategyDesignPattern2/StrategyDesignPattern2/TaxiTravelStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyDesignPattern2
+{
+    class TaxiTravelStrategy : ITravelStrategy
+    {
+        public void GotoAirport()
+        {
+            Console.WriteLine("Traveler is going to Airport by Taxi and will be charged Rs 1000");
+        }
+    }
+}
