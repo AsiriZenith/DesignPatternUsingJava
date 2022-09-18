bb24e130f9d5bdf0a2ae0e43d7043b682923ccaf
AsiriZenith <asirisenith@gmail.com>
2022-09-18 20:14:08 +0530

    Adaptor Pattern implementation

--- AdaptorPattern1.java
+++ AdaptorPattern1.java
@@ -78,8 +78,38 @@ class MediaAdapter implements MediaPlayer {
 }
 
 /**
+ * AudioPlayer
+ */
+class AudioPlayer implements MediaPlayer {
+
+    MediaAdapter mediaAdapter;
+
+    @Override
+    public void play(String audioType, String fileName) {
+
+        if (audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing mp3 file. Name: " + fileName);
+        } else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(audioType, fileName);
+        } else {
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+    }
+}
+
+/**
  * AdaptorPattern1
  */
 public class AdaptorPattern1 {
 
+    public static void main(String[] args) {
+
+        AudioPlayer audioPlayer = new AudioPlayer();
+
+        audioPlayer.play("mp3", "beyond the horizon.mp3");
+        audioPlayer.play("mp4", "alone.mp4");
+        audioPlayer.play("vlc", "far far away.vlc");
+        audioPlayer.play("avi", "mind me.avi");
+    }
 }
