b2cb0044dd5a31d7d2dc303a923013916688e70d
AsiriZenith <asirisenith@gmail.com>
2022-09-21 13:20:52 +0530

    DecoratorPattern implementation

---
+++ DecoratorPattern1.java
@@ -0,0 +1,81 @@
+/**
+ * Car
+ */
+interface Car {
+    public void assemble();
+}
+
+/**
+ * BasicCar
+ */
+class BasicCar implements Car {
+
+    @Override
+    public void assemble() {
+        System.out.println("Basic Car.");
+    }
+
+}
+
+/**
+ * CarDecorator
+ */
+class CarDecorator implements Car {
+
+    protected Car car;
+
+    public CarDecorator(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void assemble() {
+        this.car.assemble();
+    }
+
+}
+
+/**
+ * SportsCar
+ */
+class SportsCar extends CarDecorator {
+
+    public SportsCar(Car car) {
+        super(car);
+    }
+
+    public void assemble() {
+        super.assemble();
+        System.out.println("Adding features of Sports Car.");
+    }
+}
+
+/**
+ * LuxuryCar
+ */
+class LuxuryCar extends CarDecorator {
+
+    public LuxuryCar(Car car) {
+        super(car);
+    }
+
+    public void assemble() {
+        super.assemble();
+        System.out.println("Adding features of Luxury Car.");
+    }
+}
+
+/**
+ * DecoratorPattern1
+ */
+public class DecoratorPattern1 {
+
+    public static void main(String[] args) {
+        Car sportsCar = new SportsCar(new BasicCar());
+        sportsCar.assemble();
+        System.out.println("\n*****");
+
+        Car sportLuxuryCar = new SportsCar(new LuxuryCar(new BasicCar()));
+        sportLuxuryCar.assemble();
+    }
+}
