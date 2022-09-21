71d6dddc8f4eafe7085b6859c507717121d2cce1
AsiriZenith <asirisenith@gmail.com>
2022-09-21 18:48:12 +0530

    FlyweightPattern implementation

---
+++ FlyweightPattern/FlyweightPattern.java
@@ -0,0 +1,149 @@
+package FlyweightPattern;
+
+import java.util.HashMap;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FlyweightPattern extends JFrame {
+
+    private static final long serialVersionUID = -1350200437285282550L;
+    private final int WIDTH;
+    private final int HEIGHT;
+
+    private static final ShapeType shapes[] = { ShapeType.LINE, ShapeType.OVAL_FILL, ShapeType.OVAL_NOFILL };
+    private static final Color colors[] = { Color.RED, Color.GREEN, Color.YELLOW };
+
+    public static void main(String[] args) {
+        FlyweightPattern flyweightPattern = new FlyweightPattern(500, 600);
+    }
+
+    public FlyweightPattern(int width, int height) {
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        Container contentPane = getContentPane();
+
+        JButton startButton = new JButton("Draw");
+        final JPanel panel = new JPanel();
+
+        contentPane.add(panel, BorderLayout.CENTER);
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                Graphics g = panel.getGraphics();
+                for (int i = 0; i < 20; i++) {
+                    Shape shape = ShapeFactory.getShape(getRandomShape());
+                    shape.draw(g, getRandomX(), getRandomY(), getRandomWidth(), getRandomHeight(), getRandomColor());
+                }
+            }
+        });
+    }
+
+    private ShapeType getRandomShape() {
+        return shapes[(int) (Math.random() * shapes.length)];
+    }
+
+    private int getRandomX() {
+        return (int) (Math.random() * WIDTH);
+    }
+
+    private int getRandomY() {
+        return (int) (Math.random() * HEIGHT);
+    }
+
+    private int getRandomWidth() {
+        return (int) (Math.random() * (WIDTH / 10));
+    }
+
+    private int getRandomHeight() {
+        return (int) (Math.random() * (HEIGHT / 10));
+    }
+
+    private Color getRandomColor() {
+        return colors[(int) (Math.random() * colors.length)];
+    }
+}
+
+enum ShapeType {
+    OVAL_FILL, OVAL_NOFILL, LINE;
+}
+
+interface Shape {
+    public void draw(Graphics g, int x, int y, int width, int height, Color color);
+}
+
+class Oval implements Shape {
+
+    private boolean fill;
+
+    public Oval(boolean fill) {
+        this.fill = fill;
+        System.out.println("Creating Oval object with fill = " + fill);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(Graphics circle, int x, int y, int width, int height, Color color) {
+        circle.setColor(color);
+        circle.drawOval(x, y, width, height);
+        if (fill) {
+            circle.fillOval(x, y, width, height);
+        }
+    }
+
+}
+
+class Line implements Shape {
+
+    public Line() {
+        System.out.println("Creating Line object.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(Graphics line, int x1, int y1, int x2, int y2, Color color) {
+        line.setColor(color);
+        line.drawLine(x1, y1, x2, y2);
+    }
+
+}
+
+class ShapeFactory {
+
+    private static final HashMap<ShapeType, Shape> shapes = new HashMap<>();
+
+    public static Shape getShape(ShapeType type) {
+        Shape shapeImpl = shapes.get(type);
+
+        if (shapeImpl == null) {
+            if (type.equals(ShapeType.OVAL_FILL)) {
+                shapeImpl = new Oval(true);
+            } else if (type.equals(ShapeType.OVAL_NOFILL)) {
+                shapeImpl = new Oval(false);
+            } else if (type.equals(ShapeType.LINE)) {
+                shapeImpl = new Line();
+            }
+            shapes.put(type, shapeImpl);
+        }
+        return shapeImpl;
+    }
+}
