46defd7447873d6a7d3cf24bec6ec1809f6907ba
AsiriZenith <asirisenith@gmail.com>
2022-09-21 11:59:50 +0530

    CompositePattern1 implementation

---
+++ CompositePattern1.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Employee
+ */
+class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        this.subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [dept=" + dept + ", name=" + name + ", salary=" + salary + "]";
+    }
+
+}
+
+/**
+ * CompositePattern1
+ */
+public class CompositePattern1 {
+
+    public static void main(String[] args) {
+
+        Employee CEO = new Employee("John", "CEO", 30000);
+
+        Employee headSales = new Employee("Robert", "Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel", "Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura", "Marketing", 10000);
+        Employee clerk2 = new Employee("Rob", "Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard", "Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob", "Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        System.out.println(CEO);
+
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
